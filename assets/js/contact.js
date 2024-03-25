187c7c3c6d960fb6d80456a569b61730eda75a39
attr-cs <attr-cs@users.noreply.github.com>
2024-03-25 20:59:20 +0530

    contact page created

---
+++ assets/js/contact.js
@@ -0,0 +1,34 @@
+const submitButton = document.getElementById("submitButton");
+const textInfo = document.getElementById("textInfo");
+
+submitButton.addEventListener("click",function(event){
+    event.preventDefault();
+})
+
+let data = {};
+function onSubmit(){
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+    let comments = document.getElementById("comments");
+    let nameValue = name.value;
+    let emailValue = email.value;
+    let commentsValue = comments.value;
+    if(nameValue.trim() === "" || emailValue.trim() === ""){
+        textInfo.innerHTML = `<span id="red">Invalid Credentials!</span>`
+    }
+    else{
+        data = {
+            name:nameValue,
+            email:emailValue,
+            comments:commentsValue
+        }
+        textInfo.innerHTML = `<span id="green">Successfully submitted!</span>`
+    }
+    name.value = "";
+    email.value = "";
+    comments.value = "";
+}
+
+submitButton.onclick = ()=>{
+    onSubmit();
+}
